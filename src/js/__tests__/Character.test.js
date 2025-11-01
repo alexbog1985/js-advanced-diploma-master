ab39745b703e2c410a8a6e6799c9123d981f3b11
Aleksey Bogdanov <aleksey@bogdanov77.ru>
2025-11-01 19:00:26 +0300

    add test char classes

--- src/js/__tests__/Character.test.js
+++ src/js/__tests__/Character.test.js
@@ -12,17 +12,44 @@ describe('Character', () => {
   });
 });
 
-const inheritedClassesTestCases = [
-  { Class: Bowman, level: 1, type: 'bowman' },
-  { Class: Swordsman, level: 1, type: 'swordsman' },
-  { Class: Magician, level: 1, type: 'magician' },
-  { Class: Daemon, level: 1, type: 'daemon' },
-  { Class: Undead, level: 1, type: 'undead' },
-  { Class: Vampire, level: 1, type: 'vampire' }
+const characterClasses = [
+  { Class: Bowman, type: 'bowman', attack: 25, defence: 25 },
+  { Class: Swordsman, type: 'swordsman', attack: 40, defence: 10 },
+  { Class: Magician, type: 'magician', attack: 10, defence: 40 },
+  { Class: Daemon, type: 'daemon', attack: 10, defence: 10 },
+  { Class: Undead, type: 'undead', attack: 40, defence: 10 },
+  { Class: Vampire, type: 'vampire', attack: 25, defence: 25 },
 ];
 
-inheritedClassesTestCases.forEach(({ Class, level, type }) => {
-  test(`не должен выбрасывать исключение при создании объекта ${type}`, () => {
-    expect(() => new Class(level)).not.toThrow();
-  });
-});
+describe.each(characterClasses)(
+  'Класс $Class.name',
+  ({ Class, type, attack, defence }) => {
+    test('должен корректно создавать экземпляр', () => {
+      const character = new Class(1);
+      expect(character).toBeInstanceOf(Class);
+      expect(character).toBeInstanceOf(Character);
+    });
+
+    test('должен иметь правильные характеристики для 1 уровня', () => {
+      const character = new Class(1);
+      expect(character.level).toBe(1);
+      expect(character.type).toBe(type);
+      expect(character.attack).toBe(attack);
+      expect(character.defence).toBe(defence);
+      expect(character.health).toBe(50);
+    });
+
+    test('должен позволять задавать кастомный тип', () => {
+      const customType = 'custom-type';
+      const character = new Class(1, customType);
+      expect(character.type).toBe(customType);
+    });
+
+    const levelTestCases = [2, 3, 4];
+    test.each(levelTestCases)('должен корректно создавать персонажа уровня %i', (level) => {
+      const character = new Class(level);
+      expect(character.level).toBe(level);
+    });
+  }
+);
+
